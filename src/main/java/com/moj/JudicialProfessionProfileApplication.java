c7cbdba67d59ea5a5370d84ce0f86a6ffd3d49ed
Phearun Rath <rathphearun168@gmail.com>
2017-11-01 09:05:56 +0700

    change jar to war

--- src/main/java/com/moj/JudicialProfessionProfileApplication.java
+++ src/main/java/com/moj/JudicialProfessionProfileApplication.java
@@ -2,6 +2,8 @@ package com.moj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
@@ -11,14 +13,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Import({ springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class })
 @SpringBootApplication
 @ComponentScan({ "com.moj", "com.phearun.fileupload" })
-public class JudicialProfessionProfileApplication/* extends SpringBootServletInitializer */{
+public class JudicialProfessionProfileApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JudicialProfessionProfileApplication.class, args);
 	}
 
-	/*@Override
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(JudicialProfessionProfileApplication.class);
-	}*/
+	}
 }
