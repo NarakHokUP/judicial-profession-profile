0cb4b50c5ccee1de9ad27fc412ddc1dec709ea07
Phearun Rath <rathphearun168@gmail.com>
2017-12-06 10:16:41 +0700

    added: search by en-name and kh-name

--- src/main/java/com/moj/repository/OfficerRepository.java
+++ src/main/java/com/moj/repository/OfficerRepository.java
@@ -1,11 +1,19 @@
 package com.moj.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.moj.model.Officer;
 
 @Repository
 public interface OfficerRepository extends JpaRepository<Officer, Integer>{
-
+	
+	@RestResource(path="en-name", rel="en-name")
+	public List<Officer> findByEnNameContainsIgnoreCase(@Param("enName") String enName);
+	@RestResource(path="kh-name", rel="kh-name")
+	public List<Officer> findByKhNameContainsIgnoreCase(@Param("khName") String khName);
 }
