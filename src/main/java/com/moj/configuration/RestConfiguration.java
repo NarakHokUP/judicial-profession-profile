e014ce7468a556e5074ea9752f803b5a429ad29e
Phearun Rath <rathphearun168@gmail.com>
2017-12-13 19:23:36 +0700

    updated: expose id to all entities with one time configuration

--- src/main/java/com/moj/configuration/RestConfiguration.java
+++ src/main/java/com/moj/configuration/RestConfiguration.java
@@ -1,19 +1,34 @@
 package com.moj.configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
-import com.moj.model.Officer;
-import com.moj.model.Position;
-
 @Configuration
 public class RestConfiguration extends RepositoryRestConfigurerAdapter{
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		config.exposeIdsFor(Officer.class);
-		config.exposeIdsFor(Position.class);
+		
+		//config.exposeIdsFor(Officer.class);
+		//config.exposeIdsFor(Position.class);
+		
+		//TODO: Expose id for all entities!
+		entityManager.getMetamodel().getEntities().forEach(entity->{
+			try {
+				System.out.println("Model: " + entity.getName());
+				Class<? extends Object> clazz = Class.forName(String.format("com.moj.model.%s", entity.getName()));
+				config.exposeIdsFor(clazz);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		});
 	}
-
 }
+
